ba4be95935ee479ff814f0f9a1922352e78eaa58
gnawinganimal <unrongeur@proton.me>
2023-04-17 13:31:23 -0700

    Update arena page

--- src/pages/Arena.tsx
+++ src/pages/Arena.tsx
@@ -4,11 +4,11 @@ export function Arena() {
     return (
         <div>
 
-            <h3 className="text-2xl">Current Round</h3>
+            <h3 className="text-2xl">Current Rounds</h3>
             
             <h3 className="text-2xl">Upcoming Rounds</h3>
 
-            <h3 className="text-2xl">Finished Rounds</h3>
+            <h3 className="text-2xl">Completed Rounds</h3>
 
         </div>
     );
