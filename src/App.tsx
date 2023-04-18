ae26de067fdb776c1933244f09067ebe7924d04e
gnawinganimal <unrongeur@proton.me>
2023-04-17 17:57:01 -0700

    Add simple routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Routes } from "react-router";
+import { BrowserRouter, Route } from 'react-router-dom';
+
 
 import { Arena } from './pages/Arena';
 
 function App() {
   return (
     <div className="App">
-      <Arena />
+      <BrowserRouter>
+        <Routes>
+          <Route 
+            path="arena/:id"
+            element={<Arena />}
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
